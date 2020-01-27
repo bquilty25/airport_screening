3bc7dbc661775490c827d56212041afb9c2f736d
Billy <bquilty25@gmail.com>
2020-01-27 18:25:59 +0000

    Version 1

--- app.R
+++ app.R
@@ -10,63 +10,21 @@
 suppressPackageStartupMessages({
   library(shiny)
   library(ggplot2)
+  library(tidyverse)
   library(dplyr)
-  #devtools::install_github("liamgilbey/ggwaffle")
-  library(ggwaffle)
+  library(furrr)
   #install.packages("emojifont")
   library(emojifont)
-  #install.packages("patchwork")
   library(cowplot)
-  
+
 })
 
-# fileInput("file1", "Choose CSV File", multiple = FALSE, accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv")),
-# tags$hr(),
-# checkboxInput("header", "Header", TRUE),
-# radioButtons("sep", "Separator", choices = c(Comma = ",", Semicolon = ";", Tab = "\t"), selected = ",")
+source("utils.R")
 
-calc_probs <- function(dur.flight,mu_inc,sigma_inc,mu_inf,sigma_inf,sens.exit,sens.entry,prop.asy,sims){
-  
-  #convert to gamma parameters
-  alpha_inc=(mu_inc^2)/sigma_inc
-  beta_inc=sigma_inc/mu_inc
-  
-  #convert to gamma parameters
-  alpha_inf=(mu_inf^2)/sigma_inf
-  beta_inf=sigma_inf/mu_inf
-  
-  #convert flight time to days
-  dur_flight=dur.flight/24
-  
-  df = data.frame(incu = rgamma(sims,shape=alpha_inc,scale=beta_inc),
-                  inf = rgamma(sims,shape=alpha_inf,scale=beta_inf),
-                  flight.at = runif(sims,0,(mu_inc+mu_inf)*2)) %>% 
-    mutate(arrive.inf = flight.at < (mu_inc+mu_inf-dur_flight)) %>%
-    filter(arrive.inf == T) %>%
-    mutate(symp.at.exit = incu<flight.at) %>%
-    mutate(symp.at.entry = incu<(flight.at+dur_flight)) %>%
-    mutate(found.at.exit = symp.at.exit * (runif(n(),0,1)<sens.exit)) %>%
-    mutate(found.at.entry = symp.at.entry * (runif(n(),0,1)<sens.entry)) %>%
-    mutate(found.only.at.entry = found.at.entry-found.at.exit)
-  
-  #results
-  prob_det_exit <- ((sum(df$found.at.exit) / dim(df)[1])*(1-prop.asy)) %>% round(2) #prop found at exit
-  
-  if(sens.entry>=sens.exit){
-    prob_det_entry <- ((sum(df$found.only.at.entry) / dim(df)[1])*(1-prop.asy)) %>% round(2)  #additional prop found atentry
-    prob_undet <- 1-(prob_det_exit+prob_det_entry)
-    return(list("prob_det_exit"=prob_det_exit,"prob_det_entry"=prob_det_entry,"prob_undet"=prob_undet))
-  }
-  else{
-    prob_det_entry <-0
-    prob_undet <- 1-(prob_det_exit+prob_det_entry)
-    return(list("prob_det_exit"=prob_det_exit,"prob_det_entry"=prob_det_entry,"prob_undet"=prob_undet))
-  }
-}
 
 ui <- list(
   tags$style(type="text/css", "
-  body { padding-top: 70px; }
+  body { padding-top: 20px; padding-left: 20px; padding-right: 20px}
   .inline label.control-label, .inline .selectize-control.single { 
     display: table-cell; 
     text-align: left; 
@@ -86,88 +44,139 @@ ui <- list(
   hr { margin:5px; border-top: 1px solid grey; }
 "),
   fluidPage(
-    tags$label(class="h3","Effectiveness of airport screening at detecting infected travellers"),
+    titlePanel(""),
     sidebarLayout(
       sidebarPanel(
         #tags$label(class="h3",)
         sliderInput('dur.flight', 
-                    label = "Flight duration (hours)",
-                    min = 0.1, max = 20, step = 1, value = 10),
+                    label = "Travel duration (hours)",
+                    min = 0.1, max = 20, step = 1, value = 12),
         sliderInput(inputId = 'sens.exit',
                     label = 'Sensitivity of exit screening',
-                    value = 0.9, min = 0, max = 1, step = 0.1),
+                    value = 86, min = 0, max = 100, step = 1, post  = " %"),
         sliderInput(inputId = 'sens.entry',
                     label = 'Sensitivity of entry screening',
-                    value = 0.9, min = 0, max = 1, step = 0.1),
+                    value = 86, min = 0, max = 100, step = 1, post  = " %"),
+        sliderInput(inputId = 'prop.asy',
+                    label = 'Proportion of cases that are asymptomatic',
+                    value = 17, min = 0, max = 100, step = 1),
         hr(),
         div(class="header",
+            selectInput('pathogen',label='Pathogen',
+                        choices = unique(pathogen$name),
+                        selected = pathogen$name[1]),
             numericInput("mu_inc",
-                         'Days between infection and symptom onset (mean)',
-                         value = 6.7, min = 0.1, max = 20, step = 0.1),
+                         'Days from infection to symptom onset (mean)',
+                         value = 1, min = 0.1, max = 20, step = 0.1),
             numericInput("sigma_inc",
-                         "Days between infection and symptom onset (variance)",
-                         value = 16.7, min = 0.1, max = 20, step = 0.1),
+                         "Days from infection to symptom onset (variance)",
+                         value = 1, min = 0.1, max = 20, step = 0.1),
             numericInput("mu_inf",
-                         'Days between symptom onset and recovery (mean)',
-                         value = 9.3, min = 0.1, max = 20, step = 0.1),
+                         'Days from symptom onset to severe symptoms e.g hospitalisation (mean)',
+                         value = 1, min = 0.1, max = 20, step = 0.1),
             numericInput("sigma_inf",
-                         "Days between symptom onset and recovery (variance)",
-                         value = 5.2, min = 0.1, max = 20, step = 0.1)
+                         "Days from symptom onset to severe symptoms e.g hospitalisation (variance)",
+                         value = 1, min = 0.1, max = 20, step = 0.1)
         )
         
       ),
       mainPanel(
+        h1("Effectiveness of airport screening at detecting infected travellers"),
+        shiny::includeMarkdown("date_stamp.md"),
+        tabsetPanel(type = "tabs",
+                    tabPanel(title = "Plot",
+                             fluidRow(shiny::includeMarkdown("waffle_description.md")),
+                             fluidRow(uiOutput("waffle_plot"))),
+                             
+                    tabPanel(title = "Model",
+                             fluidRow(shiny::includeMarkdown("assumptions.md"))),
+                    tabPanel(title = "References",
+                             shiny::includeMarkdown("references.md"))
+          )
         
-        fluidRow(shiny::includeMarkdown("waffle_description.md")),
-        uiOutput("waffle_plot")
-        #tableOutput("testROI")
       )
     )
-  ),
-  fluidPage(title = "References",
-            fluidRow(shiny::includeMarkdown("references.md"))
-  ))
+  )
+  #fluidPage(title = "Key assumptions",
+  #          fluidRow(shiny::includeMarkdown("assumptions.md"))
+  #),
+  #fluidPage(title = "References",
+  #          fluidRow(shiny::includeMarkdown("references.md"))
+  )
 
 
 server <- function(input, output, session){
   
-  waffle_df <- reactive({
-    probs <- calc_probs(dur.flight = input$dur.flight,
-                        mu_inc=input$mu_inc,
-                        sigma_inc=input$sigma_inc,
-                        mu_inf=input$mu_inf,
-                        sigma_inf=input$sigma_inf,
-                        sens.exit=input$sens.exit,
-                        sens.entry=input$sens.entry,
-                        prop.asy=0,
-                        sims=10000)
+  observe({
+    default=input$pathogen
     
-    waffle_df <- expand.grid(y = 1:10, x = 1:10) %>% 
+      updateNumericInput(session,"mu_inc",
+                        value = pathogen %>% 
+                          filter(name==default) %>% pull(mu_inc))
+      updateNumericInput(session,"sigma_inc",
+                         value = pathogen %>% 
+                           filter(name==default) %>% pull(sigma_inc))
+      updateNumericInput(session,"mu_inf",
+                         value = pathogen %>% 
+                           filter(name==default) %>% pull(mu_inf))
+      updateNumericInput(session,"sigma_inf",
+                         value = pathogen %>% 
+                           filter(name==default) %>% pull(sigma_inf))
+      
+    }
+  )
+
+  
+  waffle_df <- reactive({
+    
+    
+    travellers <- generate_travellers(input, i = rep(10000, 1))
+    
+    probs      <- generate_probabilities(travellers)
+    
+    # round(probs$prob_det_exit,2)
+    # round(probs$prob_det_entry,2)
+    # 100 - (round(probs$prob_det_exit,2) + round(probs$prob_det_entry_only,2))
+    # 
+    waffle_labels <- data.frame(
+      desc = factor(c(rep("detected at exit screening",
+                          round(probs$prop_symp_at_exit)[1]),
+                      rep("detected as severe on flight",
+                          round(probs$prop_sev_at_entry)[1]),
+                      rep("detected at entry screening",
+                          round(probs$prop_symp_at_entry)[1]),
+                      rep("not detected",
+                          # this is here to make sure we always sum to 100
+                          #round(100*(1 - (probs$prob_det_exit + probs$prob_det_entry)),2))),
+                          100 - (round(probs$prop_symp_at_exit[1]) + 
+                                   round(probs$prop_sev_at_entry)[1] +
+                                   round(probs$prop_symp_at_entry[1])))),
+                    ordered = T)) %>%
+      mutate(
+        desc = factor(desc,
+                      levels = c("detected at exit screening",
+                                 "detected as severe on flight",
+                                 "detected at entry screening",
+                                 "not detected")))
+    
+    waffle_counts <- count(waffle_labels, desc, .drop = FALSE) %>%
+      mutate(desc_comb = paste(n, desc))
+    
+    
+    waffle_df <- expand.grid(y = -c(1:5), x = 1:20) %>% 
       as_tibble() %>% 
-      mutate(desc=forcats::as_factor(c(rep("Case found at exit screening",
-                                           round(probs$prob_det_exit*100,2)),
-                                       rep("Additional case found at entry screening",
-                                           round(probs$prob_det_entry*100,2)),
-                                       rep("Case not detected by screening",
-                                           round(probs$prob_undet*100,2))))) %>%
-      mutate(desc = factor(desc, levels = c("Case found at exit screening",
-                                            "Additional case found at entry screening",
-                                            "Case not detected by screening"),
-                           ordered = TRUE))
+      bind_cols(., waffle_labels) %>%
+      mutate(desc_comb = factor(desc, 
+                                levels = waffle_counts$desc,
+                                labels = waffle_counts$desc_comb))
     return(waffle_df)
   })
   
   nat_hist_periods <- reactive({
     #convert to gamma parameters
-    alpha_inc=(input$mu_inc^2)/input$sigma_inc
-    beta_inc=input$sigma_inc/input$mu_inc
-    
-    #convert to gamma parameters
-    alpha_inf=(input$mu_inf^2)/input$sigma_inf
-    beta_inf=input$sigma_inf/input$mu_inf
-    
-    periods <- data.frame(inc_period=rgamma(10000,shape=alpha_inc,scale=beta_inc),
-                          inf_period=rgamma(10000,shape=alpha_inf,scale=beta_inf))
+    periods <- data.frame(
+      inc_period = time_to_event(1e4, input$mu_inc, input$sigma_inc),
+      inf_period = time_to_event(1e4, input$mu_inf, input$sigma_inf))
     return(periods)
   })
   
@@ -177,22 +186,24 @@ server <- function(input, output, session){
       #mutate(group=as.factor(group),
       mutate(label=fontawesome("fa-user"))
     
-    waffle_colors <- RColorBrewer::brewer.pal(3, name = "Set2")[c(1,3,2)]
-    names(waffle_colors) <- c("Case found at exit screening",
-                              "Additional case found at entry screening",
-                              "Case not detected by screening")
+    waffle_colors <- RColorBrewer::brewer.pal(4, name = "Set2")[c(1,4,3,2)]
     
-    waffle_plot <- ggplot(waffle_data,aes(x = x, y = y, colour = desc)) + 
-      geom_raster(aes(fill = desc),
+    names(waffle_colors) <- levels(waffle_data$desc_comb)
+    
+    
+    waffle_plot <- ggplot(waffle_data,
+                          aes(x = x, y = y, colour = desc_comb)) + 
+      geom_raster(aes(fill = desc_comb),
                 alpha = 0.2) +
       geom_text(aes(label=label),
                 family="fontawesome-webfont",
-                size=6, key_glyph="rect") +
+                size=6,
+                key_glyph="rect") +
       #scale_x_continuous(expand = c(0, 0)) +
       #scale_y_continuous(expand = c(0, 0), trans = 'reverse') +
       scale_colour_manual(values = waffle_colors, drop = FALSE) +
       scale_fill_manual(values = waffle_colors, drop = FALSE) +
-      labs(title = paste0("Each person represents 1 out of 100 infected travellers \nwho could infect others at the destination"))+ 
+      labs(title = "Out of 100 infected travellers:")+ 
       coord_equal()+
       theme_void()+
       theme(axis.text       = element_blank(),
@@ -200,23 +211,25 @@ server <- function(input, output, session){
             axis.ticks      = element_blank(),
             legend.title    = element_blank(),
             legend.position = "bottom",
-            legend.text     = element_text(size=8)) +
+            plot.title = element_text(hjust = 0.5, size=15),
+            #legend.key.size = unit(3, "line"),
+            legend.text     = element_text(size=12)) +
       guides(color=guide_legend(ncol=1)) 
     
     period_plot_data <- nat_hist_periods()
     
     inc_period_plot  <- ggplot(period_plot_data,aes(x=inc_period)) + 
-      geom_density(fill = "lightskyblue",
+      geom_density(fill  = "lightskyblue",
                    color = "lightskyblue",
                    alpha = 0.5) +
       labs(x="Infection to onset (days)") + theme_minimal() +
-      ylab("Probability density")
+      ylab("Density") 
     inf_period_plot  <- ggplot(period_plot_data, aes(x=inf_period)) + 
-      geom_density(fill = "lightskyblue",
+      geom_density(fill  = "lightskyblue",
                    color = "lightskyblue",
                    alpha = 0.5) +
-      labs(x="Onset to recovery (days)") + theme_minimal() +
-      ylab("Probability density")
+      labs(x="Onset to severe (days)") + theme_minimal() +
+      ylab("Density")
     
     
     plots <- align_plots(waffle_plot, inc_period_plot,
@@ -225,20 +238,19 @@ server <- function(input, output, session){
     bottom_row <- plot_grid(plots[[2]], inf_period_plot)
     
     # then combine with the top row for final plot
-    plot_grid(plots[[1]], NULL, bottom_row, ncol = 1,
-              rel_heights = c(3,0.25,1))
+    plot_grid(plots[[1]], bottom_row, ncol = 1,
+              rel_heights = c(3,2))
     
-    
-
   },
   
-  
+
   height = function() {
     session$clientData$output_waffleplot_width
-  })
+  }
+  )
   
   output$waffle_plot <- renderUI({
-    plotOutput("waffleplot", height = input$plotSize)
+    plotOutput("waffleplot",height="auto")
   })
 }
 
